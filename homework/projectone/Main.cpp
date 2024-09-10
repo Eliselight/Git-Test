81e5814e384f03d1f482e8e7e7baf97d0e07d636
Eliselight <llightn1@ut.utm.edu>
2024-09-10 10:20:20 -0500

    Hello World

---
+++ homework/projectone/Main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file Main.cpp
+ * @author Elise Lightner
+ * @date 2024-09-10
+ * @brief building a hello world program
+ * 
+ * practice program for refuser
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  
+  return 0;
+}
