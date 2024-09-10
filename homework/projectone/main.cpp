1a803430bb29a3337c8f642ea0737bed04f80669
Eliselight <llightn1@ut.utm.edu>
2024-09-10 10:09:58 -0500

    first main.cpp

---
+++ homework/projectone/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file main.cpp
+ * @author Elise Lightner
+ * @date 2024-09-10
+ * @brief simple hello world program
+ * 
+ * creating a hello world program to display abilities?
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+  
+  return 0;
+}
