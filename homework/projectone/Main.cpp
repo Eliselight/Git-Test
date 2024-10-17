6075e5cd164d1cbda66d49403f1ca745fcdd3a3d
Eliselight <llightn1@ut.utm.edu>
2024-10-17 11:54:02 -0500

    Main

--- homework/projectone/Main.cpp
+++ homework/projectone/Main.cpp
@@ -9,11 +9,29 @@
 
 
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
+
+int count(int n);
 int main() {
+  int number;
+ 
+ 
   cout << "Hello World!" << endl;
+  cout << "Please enter a number: ";
+  cin >> number;
+  cout << "You chose " << number << " as your number." << endl;
   
+int total = count(number);    
+  cout << "The sum of numbers from 1 to " << number << " is: " << total << endl;
   return 0;
 }
+int count(int n){
+  int total = 0; 
+    for (int i = 0; i <= n; i++) {
+        total += i;  
+    }
+    return total; 
+}
+
